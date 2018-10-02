531f6fd751ebf3c95babb644d104a30f5f0c9b0b
jvalduvieco <jvalduvieco@gmail.com>
2018-10-02 19:04:36 +0200

    First try to 5 hour lamps row

--- src/berlin_clock_kata/core.clj
+++ src/berlin_clock_kata/core.clj
@@ -4,3 +4,10 @@
   "Returns true when light must be on"
   [seconds]
   (even? seconds))
+
+(defn five_hour_row
+  "presents 5 hour blocks and is made up of 4 red lamps"
+  [hours]
+  (let [modulo (mod hours 5) ]
+    [(concat (repeat modulo true) (repeat (- 4 modulo) false)) ])
+  )
--- test/berlin_clock_kata/core_test.clj
+++ test/berlin_clock_kata/core_test.clj
@@ -13,3 +13,7 @@
 (deftest seconds_lamp_is_switched_on_on_zero
   (testing "On odd seconds, lamp is off"
     (is (= (seconds-lamp 0) true))))
+
+(deftest five_hour_row_has_first_light_on_at_1
+  (testing "Five hour row has first light on at 1"
+    (is (= (five_hour_row 1) [true false false false]))))
